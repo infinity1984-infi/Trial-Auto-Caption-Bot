b73214fdae9c8110d9436a97038270106d3f3f95
infinity1984-infi <ind.agamer7@gmail.com>
2025-04-15 20:39:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,4 +2,4 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()  # load environment variables from .env file
-TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")  # ensure TELEGRAM_BOT_TOKEN is set in your .env
+TOKEN = os.getenv("YOUR_BOT_TOKEN")  # ensure TELEGRAM_BOT_TOKEN is set in your .env
