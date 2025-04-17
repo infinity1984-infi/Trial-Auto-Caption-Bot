33f9d220de9119d671da14348fa44f45732c8c34
infinity1984-infi <ind.agamer7@gmail.com>
2025-04-17 18:15:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,12 @@
 # config.py
-BOT_TOKEN = "YOUR_BOT_TOKEN"
+
+# Your Telegram Bot API token (from @BotFather)
+BOT_TOKEN = 'YOUR_BOT_TOKEN_HERE'
+
+# Default qualities for each batch of videos
+DEFAULT_QUALITIES = ['480p', '720p', '1080p']
+
+# HTML caption template with placeholders
+DEFAULT_CAPTION = (
+    "<b>[@Rear_Animes]</b> <i>{title} S{season}E{episode} - {quality}</i>\n"
+)
