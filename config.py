0e0540b968159e19f9a0725d39992cd780d97512
infinity1984-infi <ind.agamer7@gmail.com>
2025-04-17 22:32:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,6 +7,6 @@ BOT_TOKEN = 'YOUR_BOT_TOKEN_HERE'
 DEFAULT_QUALITIES = ['480p', '720p', '1080p']
 
 # HTML caption template with placeholders
-DEFAULT_CAPTION = (
+DEFAULT_FORMAT = (
     "<b>[@Rear_Animes]</b> <b><i>{title} S{season}E{episode} - {quality}</i></b>"
 )
